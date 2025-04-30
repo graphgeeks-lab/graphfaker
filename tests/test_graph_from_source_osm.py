3241d4f70189469aea8c83d79e542ddb2ffa1555
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2025-04-30 23:58:03 +0100

    #13 Test OSMGraphFetcher to fetch osm data

--- tests/test_graph_from_source_osm.py
+++ tests/test_graph_from_source_osm.py
@@ -14,6 +14,8 @@ def test_graph_from_source_osm_address():
 
     assert G.is_multigraph() == True, "Value was False, should be True"
 
+    assert G.graph['created_with'] == 'OSMnx 2.0.2', "Value wasn't 'OSMnx 2.0.2', osmnx much have been updated"
+
     assert G.number_of_nodes() <= 100, "Number of nodes should be less than or equal to 100, except it have changed"
 
     assert G.number_of_edges() >= 195, "Number of edges should be equal or greater than 94, except something changed"
@@ -24,6 +26,8 @@ def test_graph_from_source_osm_place():
     assert G.is_directed() == True, "Value was False, should be True"
 
     assert G.is_multigraph() == True, "Value was False, should be True"
+
+    assert G.graph['created_with'] == 'OSMnx 2.0.2', "Value wasn't 'OSMnx 2.0.2', osmnx much have been updated"
 
     assert G.number_of_nodes() == 50, "Number of nodes should be 50, except it have changed"
 
